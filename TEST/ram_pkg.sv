789a1f8fbc950d73de4a042ea2c2d3fd684e6f43
HackAnshul <apptechyguy@gmail.com>
2025-06-16 18:56:22 +0530

    environment finished

--- TEST/ram_pkg.sv
+++ TEST/ram_pkg.sv
@@ -5,8 +5,31 @@
 `include "ram_inf.sv"
 
 package ram_pkg;
+
+  // uvm
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+   // env
   `include "ram_r_trans.sv"
   `include "ram_w_trans.sv"
+
+  `include "ram_r_seq.sv"
+  `include "ram_w_seq.sv"
+
+  `include "ram_r_seqr.sv"
+  `include "ram_w_seqr.sv"
+
+  `include "ram_r_drv.sv"
+  `include "ram_w_drv.sv"
+
+  `include "ram_r_mon.sv"
+  `include "ram_w_mon.sv"
+
+  `include "ram_r_agt.sv"
+  `include "ram_w_agt.sv"
+
+  `include "ram_sb.sv"
   `include "ram_env.sv"
 
   //testcases
